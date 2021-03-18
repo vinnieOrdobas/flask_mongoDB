805db60f83f4803c8ca3dd00cf75adeabced0311
vinnieOrdobas <viniordobas@icloud.com>
2021-03-18 13:19:46 +0000

    Update task into the database

--- app.py
+++ app.py
@@ -46,7 +46,7 @@ def register():
         # put the new user into 'session' cookie
         session['user'] = request.form.get('username').lower()
         flash('Registration Successful')
-        return redirect(url_for('profile'), username=session['user'])
+        return redirect(url_for('profile', username=session['user']))
     return render_template('register.html')
 
 
@@ -117,6 +117,20 @@ def add_task():
 
 @app.route("/edit_task/<task_id>", methods=['GET', 'POST'])
 def edit_task(task_id):
+
+    if request.method == 'POST':
+        is_urgent = "on" if request.form.get('is_urgent') else "off"
+        submit = {
+            'category_name': request.form.get('category_name'),
+            'task_name': request.form.get('task_name'),
+            'task_description': request.form.get('task_description'),
+            'is_urgent': is_urgent,
+            'due_date': request.form.get('due_date'),
+            "created_by": session['user']
+        }
+        mongo.db.tasks.update({"_id": ObjectId(task_id)}, submit)
+        flash('Task Successfully Updated')
+
     task = mongo.db.tasks.find_one({'_id': ObjectId(task_id)})
     categories = mongo.db.categories.find().sort("category_name", 1)
     return render_template('edit_task.html', task=task, categories=categories)
